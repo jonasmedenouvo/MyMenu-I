a9c199a3826c5d133db87b8a570776960b95d2b3
jonasmedenouvo <jonas.medenouvo@lapiscine.pro>
2018-08-03 12:51:34 +0200

    Add behavior in Order:
    Overall Improvments

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -1,5 +1,7 @@
 package com.ocr.anthony;
 
+import java.util.Scanner;
+
 public class Order {
     /**
      * Display all available menus in the restaurant.
@@ -12,6 +14,9 @@ public class Order {
         System.out.println("3 - Végétarien");
         System.out.println("Que souhaitez-vous comme menu ?");
 
+
+
+
     }
     /**
      * Display a selected menu.
@@ -19,7 +24,21 @@ public class Order {
      */
     public void displaySelectedMenu(int nbMenu) {
 
+        //for (nbMenu=0; nbMenu <4; nbMenu++){
 
-        System.out.println("Vous avez choisi le menu " + nbMenu);
+
+            if ((nbMenu < 1) || (nbMenu > 3)){
+                System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
+            }
+            else if (nbMenu == 1){
+                System.out.println("Vous avez choisi comme menu : poulet");
+            }else if (nbMenu == 2){
+                System.out.println("Vous avez choisi comme menu : boeuf");
+            }else if (nbMenu == 3){
+                System.out.println("Vous avez choisi comme menu : végétarien");
+            }
+        }
+
+        //System.out.println("Vous avez choisi le menu " + nbMenu);
     }
-}
+//}
